bb4503ffd1c0dbeba8b33dc348895b1b46b1c4cb
Mr-EmPee <manuel.palumbo@email.com>
2022-10-09 11:28:15 +0200

    Improvement: Backward compatibility to Java 8

--- src/main/java/ml/empee/notifier/SimpleNotifier.java
+++ src/main/java/ml/empee/notifier/SimpleNotifier.java
@@ -1,71 +1,89 @@
 package ml.empee.notifier;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ml.empee.notifier.configs.DefaultConfig;
 import ml.empee.notifier.notifiers.DefaultNotifier;
 import ml.empee.notifier.notifiers.Notifier;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+public final class SimpleNotifier implements Listener {
 
-public final class SimpleNotifier {
+  private static SimpleNotifier instance;
+  private final JavaPlugin plugin;
 
-    private static SimpleNotifier instance;
-    public static SimpleNotifier getInstance(String pluginID, JavaPlugin plugin) {
-        if(instance == null) {
-            instance = new SimpleNotifier(pluginID, plugin);
-        }
+  private final CloseableHttpClient httpClient = HttpClients.createDefault();
+  private final DefaultConfig config;
+  private final String url;
+  private final Notifier notifier;
 
-        return instance;
+  public static SimpleNotifier getInstance(String pluginID, JavaPlugin plugin) {
+    if (instance == null) {
+      instance = new SimpleNotifier(pluginID, plugin);
+      plugin.getServer().getPluginManager().registerEvents(instance, plugin);
     }
 
-    public static void checkNotifications(String pluginID, JavaPlugin plugin) {
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, getInstance(pluginID, plugin)::checkNotifications);
+    return instance;
+  }
+
+  public static void checkNotifications(String pluginID, JavaPlugin plugin) {
+    Bukkit.getScheduler().runTaskAsynchronously(plugin, getInstance(pluginID, plugin)::checkNotifications);
+  }
+
+  private SimpleNotifier(String pluginID, JavaPlugin plugin) {
+    this(pluginID, new DefaultNotifier(plugin.getLogger()), plugin);
+  }
+
+  private SimpleNotifier(String pluginID, Notifier notifier, JavaPlugin plugin) {
+    this.config = new DefaultConfig(plugin);
+
+    this.plugin = plugin;
+    this.notifier = notifier;
+    this.url = String.format(
+        "https://minecraft-api.empee.workers.dev/notifications?plugin-id=%s&plugin-version=%s&report-level=%s",
+        pluginID, plugin.getDescription().getVersion(), config.getReportLevel().ordinal()
+    );
+  }
+
+  public void checkNotifications() {
+    if (Boolean.FALSE.equals(config.getEnabled())) {
+      return;
     }
 
+    HttpGet request = new HttpGet(url);
+    try (CloseableHttpResponse response = httpClient.execute(request)) {
+      HttpEntity entity = response.getEntity();
 
-    private final JavaPlugin plugin;
+      if (entity != null) {
+        notifier.notify(EntityUtils.toString(entity));
+      }
 
-    private final DefaultConfig config;
-    private final String pluginID;
-    private final Notifier notifier;
-
-    private SimpleNotifier(String pluginID, JavaPlugin plugin) {
-        this(pluginID, new DefaultNotifier(plugin.getLogger()), plugin);
-    }
-    private SimpleNotifier(String pluginID, Notifier notifier, JavaPlugin plugin) {
-        this.config = new DefaultConfig(plugin);
-
-        this.pluginID = pluginID;
-        this.plugin = plugin;
-        this.notifier = notifier;
+    } catch (IOException e) {
+      notifier.handleException(e);
     }
 
-    public void checkNotifications() {
-        if(!config.getEnabled()) {
-            return;
-        }
+  }
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(
-                URI.create("https://minecraft-api.empee.workers.dev/notifications?"
-                    + "plugin-id=" + pluginID
-                    + "&plugin-version=" + plugin.getDescription().getVersion()
-                    + "&report-level=" + config.getReportLevel().ordinal()
-                )
-        ).build();
-
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            notifier.notify(response.body());
-        } catch (IOException | InterruptedException e) {
-            notifier.handleException(e);
-        }
-
+  @EventHandler
+  public void onServerStop(PluginDisableEvent event) {
+    if (event.getPlugin().equals(plugin)) {
+      try {
+        httpClient.close();
+      } catch (IOException e) {
+        notifier.handleException(e);
+      }
     }
+  }
+
 }
