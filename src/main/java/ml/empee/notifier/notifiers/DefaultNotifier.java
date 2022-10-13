e380767db21aece37f33500467f1f27a35a38db6
Mr-EmPee <manuel.palumbo@email.com>
2022-10-13 20:31:01 +0200

    Improvement: Message appearance

--- src/main/java/ml/empee/notifier/notifiers/DefaultNotifier.java
+++ src/main/java/ml/empee/notifier/notifiers/DefaultNotifier.java
@@ -1,5 +1,6 @@
 package ml.empee.notifier.notifiers;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import ml.empee.notifier.exceptions.SimpleNotifierException;
@@ -15,9 +16,38 @@ public final class DefaultNotifier implements Notifier {
   @Override
   public void notify(String changelog) {
     if(!changelog.isEmpty()) {
-      changelog = "This plugin isn't up to date. Here is the changelog: \n" + changelog;
-      logger.info(changelog);
+      changelog = "Your plugin is outdated! Please update it to the latest version\n" +
+                  "Info on what's changed: \n" + changelog;
+      String[] messages = changelog.split("\n");
+      String separator = buildSeparator(messages);
+
+      logger.warning(separator);
+      for(String message : messages) {
+        logger.log(Level.WARNING, "* {0}", message);
+      }
+      logger.warning(separator);
     }
+  }
+
+  private String buildSeparator(String[] lines) {
+    StringBuilder separator = new StringBuilder();
+
+    int length = findMostLength(lines);
+    for(int i=0; i<length; i++) {
+      separator.append("*");
+    }
+
+    return separator.toString();
+  }
+
+  private int findMostLength(String[] lines) {
+    int maxLength = 0;
+    for(String line : lines) {
+      if(line.length() > maxLength) {
+        maxLength = line.length();
+      }
+    }
+    return maxLength;
   }
 
   @Override
